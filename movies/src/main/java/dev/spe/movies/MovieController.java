e042fc4e44fe641925600901ba18c5fcd42bc1c4
Shreyansh Rai <shreyanshrajeevarai@gmail.com>
2023-11-30 14:57:43 +0530

    added logs

--- movies/src/main/java/dev/spe/movies/MovieController.java
+++ movies/src/main/java/dev/spe/movies/MovieController.java
@@ -1,5 +1,7 @@
 package dev.spe.movies;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,17 +16,22 @@ import java.util.Optional;
 @CrossOrigin(origins = "*")
 @RequestMapping("api/v1/movies")
 public class MovieController {
+    private static final Logger logger =
+            LogManager.getLogger(MovieController.class);
     @Autowired
     private MovieService movieService;
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies()
     {
+        logger.info("Get all movies executed");
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
 
     @GetMapping("/{imdbId}") //this could be anything in the {} but to be consistent.
     public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId)
     {
+        String log = "Single Movie queried : imdbId " + imdbId;
+        logger.info(log);
         return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId), HttpStatus.OK);
     }
 }
--- movies/src/main/java/dev/spe/movies/ReviewController.java
+++ movies/src/main/java/dev/spe/movies/ReviewController.java
@@ -1,5 +1,7 @@
 package dev.spe.movies;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +13,16 @@ import java.util.Map;
 @RequestMapping("/api/v1/reviews")
 @CrossOrigin(origins = "*")
 public class ReviewController {
+    private static final Logger logger =
+            LogManager.getLogger(ReviewController.class);
     @Autowired
     private ReviewService reviewService;
 
     //any post to the above endpoint api-v1-movies
     @PostMapping
     public ResponseEntity<Review> createReview(@RequestBody Map<String,String> payload){
+        String log = "review entered for movie" + payload.get("imdbId") + " and body " + payload.get("reviewBody");
+        logger.info(log);
         //this tells the framework to insert the request body into a map of string to string.
         return new ResponseEntity<Review>( reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
 
